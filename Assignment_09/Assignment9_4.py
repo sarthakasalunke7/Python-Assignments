f97f38cd2a237943eab322a98e7c6c75451269e5
sarthakasalunke7 <sarthakasalunke7@gmail.com>
2026-01-23 17:50:39 +0530

    Add files via upload

---
+++ Assignment_09/Assignment9_4.py
@@ -0,0 +1,10 @@
+# Write a program which accepts one number and prints cube of that number.
+
+def main():
+    print("Enter a number : ")
+    No = int(input())
+
+    print(No**3)    # No*No*No
+
+if __name__ == "__main__":
+    main()
